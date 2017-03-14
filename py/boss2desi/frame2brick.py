4042b62913ab1e7496101ca43784af25e6926d35
ngbusca <ngbusca@apc.in2p3.fr>
2017-03-14 10:53:13 -0600

    included forgotten svd

--- py/boss2desi/frame2brick.py
+++ py/boss2desi/frame2brick.py
@@ -67,8 +67,8 @@ class brick:
             if wgood.sum()==0:
                 re.append(sp.zeros([2,nbins]))
                 continue
-            f,i,r = util.spectro_perf(fl[fib,wlam],iv[fib,wlam],res[wgood,:],log=log)
-            #f,i,r = util.svd_spectro_perf(fl[fib,wlam],iv[fib,wlam],res[wgood,:],log=log)
+            #f,i,r = util.spectro_perf(fl[fib,wlam],iv[fib,wlam],res[wgood,:],log=log)
+            f,i,r = util.svd_spectro_perf(fl[fib,wlam],iv[fib,wlam],res[wgood,:],log=log)
             flux[fib,wgood]=f
             ivar[fib,wgood]=i
             reso = sp.zeros([r.shape[0],nbins])
